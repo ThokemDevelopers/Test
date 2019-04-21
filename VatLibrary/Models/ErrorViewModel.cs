86babf9cca146ed75c6b604c0c6a3d6275c64d77
aikemefuna <aikemefuna3@gmail.com>
2019-04-20 20:48:18 -0700

    Add project files.

---
+++ VatLibrary/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace VatLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
